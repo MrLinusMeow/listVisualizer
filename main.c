3b1860fb250023c52013d81632596c884e0a9df4
Meow <mrlinusmeow@skiff.com>
2024-07-31 10:16:37 +0200

    added delete function; reformated printing process; head stores size of list; z renamed to butt; improved main() for loop (not hardcoded size of list to break loop);

--- main.c
+++ main.c
@@ -7,20 +7,21 @@ typedef struct node {
 } node;
 typedef node** list;
 
-list initList();
-
 list initList(){
 	node* head = (node*) malloc(sizeof *head);
-	node* z = (node*) malloc(sizeof *z);
-	node** list = (node**)malloc((sizeof head) + (sizeof z));
-	head->next = z;
-	head->key = 'S';
+	node* butt = (node*) malloc(sizeof *butt);
+	node** list = (node**)malloc((sizeof head) + (sizeof butt));
 
-	z->next = z;
-	z->key = 'l';
+	head->next = butt;
+	head->key = 1;
+
+	butt->next = butt;
+	butt->key = '\0';
 
 	list[0] = head;
-	list[1] = z;
+	list[1] = butt;
+	printf("\thead: %x \e[92;1m[%p]\e[0m->\e[91;1m[%p]\e[0m\n", list[0]->key, list[0], list[0]->next);
+	printf("\tbutt: %x \e[91;1m[%p]\e[0m->\e[91;1m[%p]\e[0m\n", list[1]->key, list[1], list[1]->next);
 	return list;
 }
 
@@ -29,44 +30,48 @@ node* addAfter( char k, node* t){
 	new->key = k;
 	new->next = t->next;
 	t->next = new;
+	node* head = (*(&t));
+	head->key += 1;
+	printf("\n\t+add %2x \e[96;1m[%p]\e[0m-> %2x \e[94;1m[%p]\e[0m\n", new->key, new, new->next->key, new->next);
+	printf("\t\e[92;1m[head key]\e[0m + 1 = %2x\n", head->key);
 	return new;
 }
 
+void delAfter( node* t){
+	printf("\t-del %2x \e[90;1m[%p]\e[0m\n", t->next->key, t->next);
+	node* x = (node*) malloc(sizeof *x);
+	x = t->next->next;
+	free(t->next);
+	t->next = x;
+	node* head = *(&t);
+	head->key -= (head->key != 0);
+	printf("\t\e[92;1m[head key]\e[0m - 1 = %2x\n", head->key);
+}
 
 int main(){
 	list l = initList();
 
+	printf("Creating list:\n");
 	l[2] = (node*)malloc(sizeof *l);
-	l[2] = addAfter('a', l[0]);
+	l[2] = addAfter('n', l[0]);
 
 	l[3] = (node*)malloc(sizeof *l);
-	l[3] = addAfter('g', l[0]);
+	l[3] = addAfter('i', l[0]);
 
 	l[4] = (node*)malloc(sizeof *l);
-	l[4] = addAfter('r', l[0]);
+	l[4] = addAfter('p', l[0]);
 
 	l[5] = (node*)malloc(sizeof *l);
-	l[5] = addAfter('e', l[0]);
-
-	printf("Creating list:\n");
-	printf("\thead: %c \e[91m[%p]\e[0m->\e[96m[%p]\e[0m\n", l[0]->key, l[0], l[1]);
-	printf("\tbutt: %c \e[96m[%p]\e[0m->\e[96m[%p]\e[0m\n", l[1]->key, l[1], l[1]->next);
-	printf("\n\t+ %c \e[92m[%p]\e[0m->\e[96m[%p]\e[0m\n", l[2]->key, l[2], l[2]->next);
-	printf("\t^ \e[91m[head]\e[0m->\e[92m[%p]\e[0m\n", l[2]);
-	printf("\n\t+ %c \e[93m[%p]\e[0m->\e[92m[%p]\e[0m\n", l[3]->key, l[3], l[3]->next);
-	printf("\t^ \e[91m[head]\e[0m->\e[93m[%p]\e[0m\n", l[3]);
-	printf("\n\t+ %c \e[94m[%p]\e[0m->\e[93m[%p]\e[0m\n", l[4]->key, l[4], l[4]->next);
-	printf("\t^ \e[91m[head]\e[0m->\e[94m[%p]\e[0m\n", l[4]);
-	printf("\n\t+ %c \e[95m[%p]\e[0m->\e[94m[%p]\e[0m\n", l[5]->key, l[5], l[5]->next);
-	printf("\t^ \e[91m[head]\e[0m->\e[95m[%p]\e[0m\n", l[5]);
+	l[5] = addAfter('s', l[0]);
+	delAfter( l[0]);
 
 	printf("\nResult:\n");
 	node* t = l[0];
 	char c = '1';
 	for(int i = 0; ; ++i){
 		if(c >= '2' && c <= '5') c = '6'-(char)i;
-		if(i == 6) {printf("\t%c \e[9%cm[%p]\e[0m\n\t...\n", t->key, c, t); break;}
-		printf("\t%c \e[9%cm[%p]\e[0m\n", t->key, c, t);
+		if(t == t->next) {printf("\t%2x \e[9%cm[%p]\e[0m\n\t...\n", t->key, c, t); break;}
+		printf("\t%2x \e[9%cm[%p]\e[0m\n", t->key, c, t);
 		printf("\t  V\n");
 		if(i==0) ++c;
 		if(i==4) c = '6';
